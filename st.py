f89aedf30385b4deea09ad869899fb75854f8044
reach-satori <mateus.sureis@gmail.com>
2020-11-02 16:49:39 +0000

    added windows/pc98 choice

--- st.py
+++ st.py
@@ -20,8 +20,14 @@ class Satorin(discord.Client):
     def __init__(self):
         discord.Client.__init__(self)
         self.prefix = "!"
-        self.toohus = [x.rstrip() for x in open("2hus.txt", "r").readlines()]
+        self.load_2hus()
         self.motifs = [x.rstrip() for x in open("motifs.txt", "r").readlines()]
+
+    def load_2hus(self):
+        toohus = [x.rstrip() for x in open("2hus.txt", "r").readlines()]
+        win = [toohu[:-2] for toohu in toohus if toohu.split()[-1] == "w"]
+        pc98 = [toohu[:-2] for toohu in toohus if toohu.split()[-1] == "p"]
+        self.toohus ={"windows":win, "pc98":pc98}
 
     async def on_ready(self):
         print('Logged in as')
@@ -35,7 +41,16 @@ class Satorin(discord.Client):
             return
 
         if message.content.startswith(f"{self.prefix}touhou"):
-            chosen = choice(self.toohus)
+            msg = message.content.split()
+            if len(msg) > 1:
+                if message.content.split()[1].lower() in ["windows", "win", "winblows"]:
+                    chosen = choice(self.toohus["windows"])
+                elif message.content.split()[1].lower() in ["pc98","pc-98"]:
+                    chosen = choice(self.toohus["pc98"])
+                else:
+                    chosen = choice(self.toohus["pc98"] + self.toohus["windows"])
+            else:
+                chosen = choice(self.toohus["pc98"] + self.toohus["windows"])
             await message.channel.send(
                     "In your heart of hearts, you're thinking of... {}!".format(chosen)
                     )
@@ -50,7 +65,7 @@ class Satorin(discord.Client):
             query = message.content.split()
             print(query)
             if len(query) < 2:
-                await message.channel.send( "Give me a term to search!".format(queue))
+                await message.channel.send("Give me a term to search!")
                 return
             query = query[1:]
             url = await get_first_utube_vid(" ".join(query))
