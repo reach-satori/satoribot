a1fab7ab2b245e064eebe0f5b10e33da5a775466
reach-satori <mateus.sureis@gmail.com>
2020-11-05 20:58:35 +0000

    dispatch instead of bad ifelse

--- st.py
+++ st.py
@@ -14,7 +14,7 @@ def escape_ansi(line):
     ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/ ]*[@-~]')
     return ansi_escape.sub('', line)
 
-async def get_first_utube_vid(query):
+def get_first_utube_vid(query):
     request = youtube.search().list(
         part="snippet",
         maxResults=5,
@@ -28,17 +28,71 @@ async def get_first_utube_vid(query):
     return "https://www.youtube.com/watch?v={}".format(items[0].get("id").get("videoId"))
 
 class Satorin(discord.Client):
+    motifs = [x.rstrip() for x in open("motifs.txt", "r").readlines()]
+    toohus = [x.rstrip() for x in open("2hus.txt", "r").readlines()] # overriden just below
+    win = [toohu[:-2] for toohu in toohus if toohu.split()[-1] == "w"]
+    pc98 = [toohu[:-2] for toohu in toohus if toohu.split()[-1] == "p"]
+    del toohus
     def __init__(self):
         discord.Client.__init__(self)
         self.prefix = "!"
-        self.load_2hus()
-        self.motifs = [x.rstrip() for x in open("motifs.txt", "r").readlines()]
+        self.dispatcho = {
+            "yt" : self.get_ytlink,
+            "touhou" : self.get_touhouchoice,
+            "2hu" : self.get_touhouchoice,
+            "motif" : self.get_motif,
+            "weather" : self.get_weather,
+            "w" : self.get_weather,
+            "aa" : self.get_aa,
+        }
 
-    def load_2hus(self):
-        toohus = [x.rstrip() for x in open("2hus.txt", "r").readlines()]
-        win = [toohu[:-2] for toohu in toohus if toohu.split()[-1] == "w"]
-        pc98 = [toohu[:-2] for toohu in toohus if toohu.split()[-1] == "p"]
-        self.toohus ={"windows":win, "pc98":pc98}
+    def get_touhouchoice(self, message):
+        msgtext = message.content
+        msg = msgtext.split()
+        if len(msg) > 1:
+            if msgtext.split()[1].lower() in ["windows", "win", "winblows"]:
+                chosen = choice(self.win)
+            elif msgtext.split()[1].lower() in ["pc98","pc-98"]:
+                chosen = choice(self.pc98)
+            else:
+                chosen = choice(self.pc98 + self.win)
+        else:
+            chosen = choice(self.pc98 + self.win)
+        return f"In your heart of hearts, you're thinking of... {chosen}!"
+
+    def get_motif(self, message):
+        return f"Your mythologic motif is {choice(self.motifs)}."
+
+    def get_weather(self, message):
+        msgtext = message.content
+        msg = msgtext.split()
+        if len(msg) == 1:
+            return "Add the name of your city to get the weather."
+        city = msgtext.split(maxsplit=1)[1].replace(" ", "+")
+        weather = requests.get(f"https://wttr.in/{city}")
+        weather = weather.content.decode("utf-8")
+        if city.lower() == "moon":
+            weather = "\n".join(weather.split("\n")[:22])
+            weather = escape_ansi(weather)
+        else:
+            weather = "\n".join(weather.split("\n")[:7])
+            weather = escape_ansi(weather)
+        return f"```{weather}```"
+
+    def get_aa(self, message):
+        aalist = glob.glob("./ascii/ascii_*")
+        chosen = choice(aalist)
+        doimage(chosen)
+
+    def get_ytlink(self, message):
+        msgtext = message.content
+        query = msgtext.split(maxsplit=1)
+        if len(query) < 2:
+            return "Give me a term to search!"
+
+        query = query[1]
+        url = get_first_utube_vid(" ".join(query))
+        return url
 
     async def on_ready(self):
         print('Logged in as')
@@ -51,62 +105,15 @@ class Satorin(discord.Client):
         if message.author == self.user:
             return
         msgtext = message.content
-
-        if msgtext.startswith(f"{self.prefix}touhou"):
-            msg = msgtext.split()
-            if len(msg) > 1:
-                if msgtext.split()[1].lower() in ["windows", "win", "winblows"]:
-                    chosen = choice(self.toohus["windows"])
-                elif msgtext.split()[1].lower() in ["pc98","pc-98"]:
-                    chosen = choice(self.toohus["pc98"])
+        if msgtext.startswith(self.prefix):
+            msgtext = msgtext[1:].split()
+            key = msgtext[0]
+            if key in self.dispatcho:
+                ret = self.dispatcho[key](message)
+                if key == "aa":
+                    await message.channel.send(file=discord.File("ascii.png"))
                 else:
-                    chosen = choice(self.toohus["pc98"] + self.toohus["windows"])
-            else:
-                chosen = choice(self.toohus["pc98"] + self.toohus["windows"])
-            await message.channel.send(
-                    "In your heart of hearts, you're thinking of... {}!".format(chosen)
-                    )
-
-        if msgtext.startswith(f"{self.prefix}w")\
-        or msgtext.startswith(f"{self.prefix}weather"):
-            if len(msgtext.split()) == 1:
-                await message.channel.send("Add the name of your city to get the weather.")
-            else:
-                city = msgtext.split(maxsplit=1)[1].replace(" ", "+")
-
-                weather = requests.get("https://wttr.in/{}".format(city))
-                weather = weather.content.decode("utf-8")
-                if city.lower() == "moon":
-                    weather = "\n".join(weather.split("\n")[:22])
-                    weather = escape_ansi(weather)
-                    await message.channel.send("```{}```".format(weather))
-                else:
-                    weather = "\n".join(weather.split("\n")[:7])
-                    weather = escape_ansi(weather)
-                    await message.channel.send("```{}```".format(weather))
-
-        if msgtext.startswith(f"{self.prefix}motif"):
-            chosen = choice(self.motifs)
-            await message.channel.send(
-                    "Your mythologic motif is...\n`{}`".format(chosen)
-                    )
-
-        if msgtext.startswith(f"{self.prefix}aa"):
-            aalist = glob.glob("./ascii/ascii_*")
-            chosen = choice(aalist)
-            print(chosen)
-            doimage(chosen)
-            await message.channel.send(file=discord.File("ascii.png"))
-
-        if msgtext.startswith(f"{self.prefix}yt"):
-            query = msgtext.split()
-            print(query)
-            if len(query) < 2:
-                await message.channel.send("Give me a term to search!")
-                return
-            query = query[1:]
-            url = await get_first_utube_vid(" ".join(query))
-            await message.channel.send(url)
+                    await message.channel.send(ret)
 
 bot = Satorin()
 
